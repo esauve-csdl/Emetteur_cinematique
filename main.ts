f721cc0ff5b1025043dcf7944a7d6885277801a1
esauve-csdl <80712004+esauve-csdl@users.noreply.github.com>
2021-11-02 21:58:16 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,17 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Yes)
-    go = 1
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.No)
-    Temps = 0
-    go = 0
-})
-let Temps = 0
+basic.showLeds(`
+    . # # # .
+    . # . . .
+    . # # . .
+    . # . . .
+    . # # # .
+    `)
 let go = 0
-basic.showIcon(IconNames.No)
-go = 0
 radio.setGroup(1)
-Temps = 0
+let Temps = 0
 let acc = 0
 basic.forever(function () {
-    if (go == 1) {
-        basic.pause(10)
-        Temps = Temps + 10
-    }
-})
-basic.forever(function () {
-    acc = input.acceleration(Dimension.Z)
+    acc = input.acceleration(Dimension.Z) * 0.0098
+    Temps = input.runningTime() / 1000
     radio.sendValue("acc", acc)
     radio.sendValue("T", Temps)
 })
