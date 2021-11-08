e3fd48719a0aa74db6d0fa38a59c5b9f3253686c
esauve-csdl <80712004+esauve-csdl@users.noreply.github.com>
2021-11-07 21:39:06 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,9 +9,20 @@ let go = 0
 radio.setGroup(1)
 let Temps = 0
 let acc = 0
+let somme = 0
+let moyenne = 0
 basic.forever(function () {
-    acc = input.acceleration(Dimension.Z) * 0.0098
+    let données: number[] = []
+    for (let index = 0; index <= 99; index++) {
+        données[index] = input.acceleration(Dimension.Y) * 0.0098
+    }
+    for (let i = 0; i <= 99; i++) {
+        somme = somme + données[i]
+    }
+    moyenne = somme / 100
+    somme = 0
     Temps = input.runningTime() / 1000
+    acc = moyenne
     radio.sendValue("acc", acc)
     radio.sendValue("T", Temps)
 })
