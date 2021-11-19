b90e6e642c77664ae19d8a16f8e08472c416d4c7
esauve-csdl <80712004+esauve-csdl@users.noreply.github.com>
2021-11-19 15:31:08 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,15 +13,15 @@ let somme = 0
 let moyenne = 0
 basic.forever(function () {
     let données: number[] = []
-    for (let index = 0; index <= 99; index++) {
+    for (let index = 0; index <= 199; index++) {
         données[index] = input.acceleration(Dimension.Y) * 0.0098
     }
-    for (let i = 0; i <= 99; i++) {
+    for (let i = 0; i <= 199; i++) {
         somme = somme + données[i]
     }
-    moyenne = somme / 100
+    moyenne = somme / 200
     somme = 0
-    Temps = input.runningTime() / 1000
+    Temps = control.eventTimestamp() / 1000000
     acc = moyenne
     radio.sendValue("acc", acc)
     radio.sendValue("T", Temps)
